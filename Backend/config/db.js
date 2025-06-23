15be03264a3e0961046be133ee3d79a39dec2aa7
piyushwattamwar <147603359+piyushwattamwar@users.noreply.github.com>
2025-06-23 12:13:11 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("❌ MongoDB connection failed", err.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
