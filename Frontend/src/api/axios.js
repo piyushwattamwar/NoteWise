db2bb25f0efa3344be1dc83d2faeb5f7eaf8ed98
piyushwattamwar <147603359+piyushwattamwar@users.noreply.github.com>
2025-06-23 11:56:33 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://notewise-6hs6.onrender.com/api", 
-  headers: { Authorization: `Bearer ${token}` } 
 });
 
 export default API;
