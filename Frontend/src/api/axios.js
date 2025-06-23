61a431d9bfbf69825a29c22b5c33a9de762686c9
piyushwattamwar <147603359+piyushwattamwar@users.noreply.github.com>
2025-06-23 11:52:32 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://notewise-6hs6.onrender.com",
-});
-
-// Automatically attach token to each request
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.authToken = token;
-  }
-  return req;
+  baseURL: "https://notewise-6hs6.onrender.com/api", 
+  headers: { Authorization: `Bearer ${token}` } 
 });
 
 export default API;
-
