935bfa7f580a62b99b0119985b3f36053cdeea3c
piyushwattamwar <147603359+piyushwattamwar@users.noreply.github.com>
2025-06-23 12:38:24 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://notewise-6hs6.onrender.com/api/auth/login", {
         email,
         password,
       });
